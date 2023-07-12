ee5a23550965ce3906b7c032decff0f39166d69d
lightScout <juanmullercs@hotmail.com>
2023-07-12 19:31:48 +0200

    add RetrofitFactory.kt, Constants.kt and update RedditService.kt

---
+++ app/src/main/java/com/lightscout/redditviewer/model/service/RedditService.kt
@@ -0,0 +1,14 @@
+package com.lightscout.redditviewer.model.service
+
+import com.lightscout.redditviewer.model.data.RedditResponse
+import com.lightscout.redditviewer.util.Constants.Companion.AFTER
+import com.lightscout.redditviewer.util.Constants.Companion.LIMIT
+import com.lightscout.redditviewer.util.Constants.Companion.LIMIT_VALUE
+import com.lightscout.redditviewer.util.Constants.Companion.PATH
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RedditService {
+    @GET(PATH)
+    suspend fun getPosts(@Query(AFTER) after : String? = null, @Query(LIMIT) limit : String = LIMIT_VALUE): RedditResponse
+}
---
+++ app/src/main/java/com/lightscout/redditviewer/util/Constants.kt
@@ -0,0 +1,15 @@
+package com.lightscout.redditviewer.util
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://www.reddit.com/"
+        const val PATH = "r/technology/new.json"
+        const val AFTER = "after"
+        const val LIMIT = "limit"
+        const val LIMIT_VALUE = "22"
+        const val FORMAT = "format"
+        const val QUERY = "q"
+        const val SUBREDDIT = "subreddit"
+        const val SUBREDDIT_VALUE = "technology"
+    }
+}
