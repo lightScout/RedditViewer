1840ac90256f3190b453b7785141a35d7d8509b3
lightScout <juanmullercs@hotmail.com>
2023-07-16 15:27:07 +0200

    remove koin and add Hilt

---
+++ app/src/main/java/com/lightscout/redditviewer/model/di/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.lightscout.redditviewer.model.di
+
+import android.app.Application
+import org.koin.dsl.module
+
+class MainApplication: Application() {
+
+    private val appModule = module {
+
+    }
+}
