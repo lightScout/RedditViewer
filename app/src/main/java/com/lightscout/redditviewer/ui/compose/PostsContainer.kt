2e2b1940642e0e33e7d4e49836a7af6a5f504f07
lightScout <juanmullercs@hotmail.com>
2023-07-19 09:33:19 +0200

    adjust listState management

--- app/src/main/java/com/lightscout/redditviewer/ui/compose/PostsContainer.kt
+++ app/src/main/java/com/lightscout/redditviewer/ui/compose/PostsContainer.kt
@@ -1,5 +1,6 @@
 package com.lightscout.redditviewer.ui.compose
 
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -25,6 +26,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -53,6 +57,7 @@ fun PostsContainer(navController: NavController) {
     val pullRefreshState = rememberPullRefreshState(isRefreshing, { viewModel.refresh() })
     val listState = rememberLazyListState()
     val isOnline = Common().isNetworkAvailable(LocalContext.current)
+
 
     LaunchedEffect(viewModel) {
         if (isOnline.not())
@@ -193,7 +198,8 @@ fun PostListOnlineView(
     LaunchedEffect(listState) {
         listState.interactionSource.interactions.collectLatest {
             val visibleItemInfo = listState.layoutInfo.visibleItemsInfo
-            if (visibleItemInfo.isNotEmpty() && visibleItemInfo.last().index >= state.data.size - 1) {
+            listState.layoutInfo.totalItemsCount
+            if (visibleItemInfo.isNotEmpty() && visibleItemInfo.last().index >= listState.layoutInfo.totalItemsCount - 1) {
                 if (isLoading.not()) {
                     viewModel.morePosts()
                 }
